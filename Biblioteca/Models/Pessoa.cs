e43898002076c615d4bd6a06869c8b0468afe6cf
VinicCosta <viniciuscodp@gmail.com>
2023-03-11 12:01:39 -0300

    Cria classe pessoa e pasta Views

---
+++ Biblioteca/Models/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Models
+{
+    public class Pessoa
+    {
+        string Nome { get; set; }
+        DateTime DataNascimento { get; set; }
+        private string CPF { get; set; }
+
+        public Pessoa()
+        {
+        }
+        public Pessoa(string nome, DateTime dataNascimento, string cpf)
+        {
+            Nome = nome;
+            DataNascimento = dataNascimento;
+            CPF = cpf;
+        }
+    }
+}
---
+++ Biblioteca/Viwes/Form1.cs
@@ -0,0 +1,10 @@
+namespace Biblioteca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
