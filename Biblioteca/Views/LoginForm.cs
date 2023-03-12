6a990b1b66d9045530a496a65884c1048ef1f8fd
Emmanuel Martins <emmanuelkta6@gmail.com>
2023-03-12 17:33:26 -0300

    Criação da tela Login

---
+++ Biblioteca/Views/LoginForm.cs
@@ -0,0 +1,15 @@
+namespace Biblioteca
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEntrar_Click(object sender, EventArgs e)
+        {
+            // valida o login
+        }
+    }
+}
