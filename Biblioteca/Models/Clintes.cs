d44f2a4cef42b2e092302117bb21bdc5c3e1b834
Gustavo <Gustavo@DESKTOP-36SHUBO>
2023-03-16 18:39:28 -0300

    Criação da Classe Clientes

---
+++ Biblioteca/Models/Clintes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Models
+{
+    public class Clintes : Pessoa
+    {
+        private int Id { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Lougradouro { get; set; }
+
+        public Clintes(int id, string bairro, string cidade, string estado, string lougradouro)
+        {
+            Id = id;
+            Bairro = bairro;
+            Cidade = cidade;
+            Estado = estado;
+            Lougradouro = lougradouro;
+        }
+
+        
+
+        
+    }
+}
