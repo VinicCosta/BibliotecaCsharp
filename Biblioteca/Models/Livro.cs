d613182d1777643b23aeb3d433499788d30818bf
Vitor Hugo Cardoso Lourenço <vitorcardoso.cataguases@hotmail.com>
2023-03-16 22:25:26 -0300

    Classe Livro

---
+++ Biblioteca/Models/Livro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Models
+{
+    public class Livro
+    {
+        public int IdLivro { get; set; }
+        public string Nome { get; set; }
+        public int Paginas { get; set; }
+        public int Quantidade { get; set; }
+
+        public string Genero { get; set; }
+
+        public Livro(int idLivro, string nome, int paginas, int quantidade, string genero)
+        {
+            IdLivro = idLivro;
+            Nome = nome;
+            Paginas = paginas;
+            Quantidade = quantidade;
+            Genero = genero;
+        }
+    }
+}
